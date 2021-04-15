603fe01fb0a22ec28811c717c473cff39fce8b2d
glekovkirilll <glekovkirill2016@gmail.com>
2021-04-16 04:20:38 +0700

    Добавлена функция просмотра содержимого портфеля пользователя с помощью команды status

--- src/main/java/TinkoffBot.java
+++ src/main/java/TinkoffBot.java
@@ -38,7 +38,6 @@ public class TinkoffBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         if(update.getMessage()!=null && update.getMessage().hasText()) {
 
-
             long chat_id = update.getMessage().getChatId();
             String str_chat_id = Long.toString(chat_id);
 
@@ -49,43 +48,81 @@ public class TinkoffBot extends TelegramLongPollingBot {
             OkHttpOpenApiFactory factory = new OkHttpOpenApiFactory(TINTOKEN, logger);
             OpenApi api = null;
 
-            boolean sandboxMode = false;
+            api = factory.createOpenApiClient(Executors.newCachedThreadPool());
+
+            /*boolean sandboxMode = false;
             if(sandboxMode) {
                 api = factory.createSandboxOpenApiClient(Executors.newCachedThreadPool());
                 ((SandboxOpenApi) api).getSandboxContext().performRegistration(null).join();
+            }//пока что не рабочее что-то
+            else {
+
+            }*/
+
+
+            if(update.getMessage().getText().toString().equals("/status")) {
+
+                ArrayList<String> portfolioStatus = new ArrayList<>();
+
+                api.getPortfolioContext().getPortfolio(api.getUserContext().getAccounts().get().accounts.get(0).brokerAccountId).get().positions.forEach(element -> {
+                    portfolioStatus.add("Figi: " + element.figi + System.lineSeparator());
+                    portfolioStatus.add(element.name + System.lineSeparator());
+                    portfolioStatus.add("Количество: " + element.balance + System.lineSeparator());
+                    portfolioStatus.add(element.averagePositionPrice + System.lineSeparator());
+                    portfolioStatus.add("=======================" + System.lineSeparator());
+                });
+
+                ArrayList<String> shortList = new ArrayList<String>();
+
+                String numberofShares = Integer.toString(portfolioStatus.size() / 5) ;
+
+                shortList.add("ВАШ ПОРТФЕЛЬ");
+                shortList.add("На данный момент у вас в портфеле " + numberofShares + " акций(я/и):" + System.lineSeparator());
+                shortList.add("=======================" + System.lineSeparator());
+
+                String PortfolioStatusList = String.join(System.lineSeparator(), portfolioStatus);
+
+                shortList.add(PortfolioStatusList);
+
+                String finalStatus = String.join(System.lineSeparator(), shortList);
+
+
+                execute(new SendMessage(str_chat_id, finalStatus));
+
+
             }
             else {
-                api = factory.createOpenApiClient(Executors.newCachedThreadPool());
+                ArrayList<String> brokList = new ArrayList<String>();
+
+                api.getMarketContext().getMarketStocks().get().instruments.forEach(element -> {
+                    brokList.add(element.figi + " " + element.name + System.lineSeparator()) ;
+                });
+
+
+                Integer numberOfElements = Integer.parseInt(update.getMessage().getText());
+
+                String startNumber = Integer.toString(numberOfElements * 10 - 10 + 1);
+                String endNumber = Integer.toString(numberOfElements * 10) ;
+
+                ArrayList<String> shortList = new ArrayList<String>();
+
+                shortList.add("Список акций");
+                shortList.add("С " + startNumber + " по " + endNumber + System.lineSeparator());
+                shortList.add("=============================" + System.lineSeparator());
+
+
+                for(int i = numberOfElements * 10 - 10; i <= numberOfElements * 10 - 1; i++) {
+                    shortList.add(brokList.get(i));
+                }
+
+
+                String shortListStr = String.join(System.lineSeparator(), shortList);
+
+
+                execute(new SendMessage(str_chat_id, shortListStr));
             }
 
-            ArrayList<String> brokList = new ArrayList<String>();
 
-            api.getMarketContext().getMarketStocks().get().instruments.forEach(element -> {
-                brokList.add(element.figi + " " + element.name + System.lineSeparator()) ;
-            });
-
-
-            Integer numberOfElements = Integer.parseInt(update.getMessage().getText());
-
-            String startNumber = Integer.toString(numberOfElements * 10 - 10);
-            String endNumber = Integer.toString(numberOfElements * 10 -1);
-
-            ArrayList<String> shortList = new ArrayList<String>();
-
-            shortList.add("List of Shares");
-            shortList.add("From " + startNumber + " to " + endNumber + System.lineSeparator());
-            shortList.add("=============================" + System.lineSeparator());
-
-
-            for(int i = numberOfElements * 10 - 10; i <= numberOfElements * 10 - 1; i++) {
-                shortList.add(brokList.get(i));
-            }
-
-
-            String shortListStr = String.join(System.lineSeparator(), shortList);
-
-
-            execute(new SendMessage(str_chat_id, shortListStr));
         }
     }
 
